c9e826bacdea23a25bcdd7929ab7bcaac9dc7377
Ohad Ravid <ohad.rv@gmail.com>
2019-01-24 08:54:16 +0100

    Added tests for malformed datetime str and some docs

--- src/datetime.rs
+++ src/datetime.rs
@@ -1,11 +1,12 @@
-use std::str::FromStr;
-use std::fmt;
 use chrono::prelude::*;
-use serde::de;
 use chrono::FixedOffset;
-use failure::Error;
+use failure::{bail, Error};
+use serde::de;
+use std::fmt;
+use std::str::FromStr;
 
-
+/// A wrapper type around chrono's DateTime, which supports parsing from VMI-format strings.
+///
 #[derive(Debug)]
 pub struct WMIDateTime(pub DateTime<FixedOffset>);
 
@@ -13,9 +14,13 @@ impl FromStr for WMIDateTime {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 21 {
+            bail!("Expected {:?} to be at least 21 chars", s)
+        }
+
         let (datetime_part, tz_part) = s.split_at(21);
 
-        let tz_min: i32= tz_part.parse()?;
+        let tz_min: i32 = tz_part.parse()?;
 
         let tz = FixedOffset::east(tz_min * 60);
 
@@ -31,15 +36,12 @@ impl<'de> de::Visitor<'de> for DateTimeVisitor {
     type Value = WMIDateTime;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            formatter,
-            "a formatted date and time string or a unix timestamp"
-        )
+        write!(formatter, "a timestamp in WMI format")
     }
 
     fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-        where
-            E: de::Error,
+    where
+        E: de::Error,
     {
         value.parse().map_err(|err| E::custom(format!("{}", err)))
     }
@@ -47,16 +49,15 @@ impl<'de> de::Visitor<'de> for DateTimeVisitor {
 
 impl<'de> de::Deserialize<'de> for WMIDateTime {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: de::Deserializer<'de>,
+    where
+        D: de::Deserializer<'de>,
     {
         deserializer.deserialize_str(DateTimeVisitor)
     }
 }
 
-#[allow(non_camel_case_types)]
 mod tests {
-    use super::*;
+    use super::WMIDateTime;
 
     #[test]
     fn it_works_with_negative_offset() {
@@ -70,5 +71,19 @@ mod tests {
         let dt: WMIDateTime = "20190113200517.500000+060".parse().unwrap();
 
         assert_eq!(dt.0.to_rfc3339(), "2019-01-13T20:05:17.000500+01:00");
+    }
+
+    #[test]
+    fn it_fails_with_malformed_str() {
+        let dt_res: Result<WMIDateTime, _> = "20190113200517".parse();
+
+        assert!(dt_res.is_err());
+    }
+
+    #[test]
+    fn it_fails_with_malformed_str_with_no_tz() {
+        let dt_res: Result<WMIDateTime, _> = "20190113200517.000500".parse();
+
+        assert!(dt_res.is_err());
     }
 }
