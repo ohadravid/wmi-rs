131cbaa9479c60d8f6fa35064cee947c5372fad3
Ohad Ravid <ohad.rv@gmail.com>
2019-01-24 09:23:28 +0100

    Rename vars, cleanup a bit

--- src/safearray.rs
+++ src/safearray.rs
@@ -12,6 +12,7 @@ use winapi::{
     um::{oaidl::SAFEARRAY, oleauto::SafeArrayAccessData, oleauto::SafeArrayUnaccessData},
 };
 
+// TODO: This should be part of winapi-rs.
 extern "system" {
     pub fn SafeArrayGetLBound(psa: *mut SAFEARRAY, nDim: UINT, plLbound: *mut LONG) -> HRESULT;
 
@@ -22,26 +23,28 @@ extern "system" {
 
 pub fn get_string_array(arr: *mut SAFEARRAY) -> Result<Vec<String>, Error> {
     let mut p_data = NULL;
-    let mut lstart: i32 = 0;
-    let mut lend: i32 = 0;
+
+    let mut lower_bound: i32 = 0;
+    let mut upper_bound: i32 = 0;
 
     unsafe {
-        check_hres(SafeArrayGetLBound(arr, 1, &mut lstart as _))?;
-        check_hres(SafeArrayGetUBound(arr, 1, &mut lend as _))?;
+        check_hres(SafeArrayGetLBound(arr, 1, &mut lower_bound as _))?;
+        check_hres(SafeArrayGetUBound(arr, 1, &mut upper_bound as _))?;
         check_hres(SafeArrayAccessData(arr, &mut p_data))?;
     }
 
+    // We know that we expect an array of BSTRs.
     let p_data: *mut BSTR = p_data as _;
 
-    // lend can be -1, in which case the array is empty and we will do nothing.
-    let data_slice = unsafe { slice::from_raw_parts(p_data, (lend + 1) as usize) };
+    // upper_bound can be -1, in which case the array is empty and we will do nothing.
+    let data_slice = unsafe { slice::from_raw_parts(p_data, (upper_bound + 1) as usize) };
 
-    let mut props = vec![];
+    let mut items = vec![];
 
-    for prop_name_bstr in data_slice[(lstart as usize)..].iter() {
-        let prop_name: &WideCStr = unsafe { WideCStr::from_ptr_str(*prop_name_bstr) };
+    for item_bstr in data_slice[(lower_bound as usize)..].iter() {
+        let item: &WideCStr = unsafe { WideCStr::from_ptr_str(*item_bstr) };
 
-        props.push(prop_name.to_string()?)
+        items.push(item.to_string()?)
     }
 
     // TODO: Make sure this happens even on errors.
@@ -49,5 +52,5 @@ pub fn get_string_array(arr: *mut SAFEARRAY) -> Result<Vec<String>, Error> {
         check_hres(SafeArrayUnaccessData(arr))?;
     }
 
-    Ok(props)
+    Ok(items)
 }
