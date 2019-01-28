d1ae869e99df83c82a5782022c0332ea29494af9
Ohad Ravid <ohad.rv@gmail.com>
2019-01-28 19:41:28 +0100

    More docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,94 @@
+//! # WMI-rs
+//!
+//! [WMI] is a management API for Windows-based operating systems.
+//! This crate provides a high level Rust API focused around data retrieval (vs. making changes to
+//! the system and watching for event which are also supported by WMI).
+//!
+//! This crate also uses `serde` to transform pointers to WMI class objects into plain Rust structs.
+//!
+//! All data is copied to Owning data structures, so the final structs are not tied in any way to
+//! the original WMI object (refer to MSDN's [Creating a WMI Application Using C++] to learn more about how data is handled by WMI).
+//!
+//! Before using WMI, a connection must be created.
+//!
+//! ```rust
+//! use wmi::{COMLibrary, WMIConnection};
+//! let com_con = COMLibrary::new().unwrap();
+//! let wmi_con = WMIConnection::new(com_con.into()).unwrap();
+//! ```
+//!
+//! There are multiple ways to get data from the OS using this crate.
+//!
+//! # Operating on untyped Variants
+//!
+//! WMI data model is based on COM's [`VARIANT`] Type, which is a struct capable of holding
+//! many types of data.
+//!
+//! This crate provides the analogous [`wmi::Variant`][Variant] enum.
+//!
+//! Using this enum, we can execute a simple WMI query and inspect the results.
+//!
+//! ```edition2018
+//! # use wmi::*;
+//! # let wmi_con = WMIConnection::new(COMLibrary::new().unwrap().into()).unwrap();
+//! use std::collections::HashMap;
+//! use wmi::Variant;
+//! let results: Vec<HashMap<String, Variant>> = wmi_con.raw_query("SELECT * FROM Win32_OperatingSystem").unwrap();
+//!
+//! for os in results {
+//!     println!("{:#?}", os);
+//! }
+//! ```
+//!
+//! # Using strongly typed data structures
+//!
+//! Using `serde`, it is possible to return a struct representing the the data.
+//!
+//! ```edition2018
+//! # use wmi::*;
+//! # let wmi_con = WMIConnection::new(COMLibrary::new().unwrap().into()).unwrap();
+//! use serde::Deserialize;
+//! use wmi::WMIDateTime;
+//!
+//! #[derive(Deserialize, Debug)]
+//! #[serde(rename = "Win32_OperatingSystem")]
+//! #[serde(rename_all = "PascalCase")]
+//! struct OperatingSystem {
+//!     caption: String,
+//!     debug: bool,
+//!     last_boot_up_time: WMIDateTime,
+//! }
+//!
+//! let results: Vec<OperatingSystem> = wmi_con.query().unwrap();
+//!
+//! for os in results {
+//!     println!("{:#?}", os);
+//! }
+//! ```
+//!
+//! Because the name of the struct given to `serde` matches the [WMI class] name, the SQL query
+//! is inferred automatically.
+//!
+//! [WMI]: https://docs.microsoft.com/en-us/windows/desktop/wmisdk/about-wmi
+//! [Creating a WMI Application Using C++]: https://docs.microsoft.com/en-us/windows/desktop/wmisdk/creating-a-wmi-application-using-c-
+//! [`VARIANT`]: https://docs.microsoft.com/en-us/windows/desktop/api/oaidl/ns-oaidl-tagvariant
+//! [WMI class] https://docs.microsoft.com/en-us/windows/desktop/cimwin32prov/win32-operatingsystem
+//!
+//! # Internals
+//!
+//! [`wmi::WMIConnection`][WMIConnection] is used to create and execute a WMI query, returning
+//! [`wmi::query::IWbemClassWrapper`][IWbemClassWrapper] which is a wrapper for a WMI object pointer.
+//!
+//! Then, [`wmi::from_wbem_class_obj`][from_wbem_class_obj] is used to create a Rust struct with the equivalent data.
+//!
+//! Deserializing data from WMI and into Rust is done via `serde` and is implemented in the [`wmi::de`][de] module.
+//! More info can be found in `serde`'s documentation about [writing a data format].
+//!
+//! [writing a data format] https://serde.rs/data-format.html
+//!
+//! There are two main data structures (other than pointers to object) which are deserialize into Rust:
+//! [`wmi::Variant`][Variant] and [`wmi::safearray::SafeArrayAccessor`][SafeArray].
+//!
 #![feature(ptr_internals, custom_attribute)]
 
 pub mod connection;
