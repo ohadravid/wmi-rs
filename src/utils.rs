59b3e02f995064a07de44f7fbb08b54f4973f0fa
Edmond Begumisa <edmond.begumisa@gmail.com>
2025-03-15 09:31:50 +1100

    Display potentially localised HRESULT message.

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,19 @@
+use super::hres;
 use serde::{de, ser};
 use std::fmt::{Debug, Display};
 use thiserror::Error;
-use super::hres;
 
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum WMIError {
     /// You can find a useful resource for decoding error codes [here](https://docs.microsoft.com/en-us/windows/win32/wmisdk/wmi-error-constants)
     /// (or a github version [here](https://github.com/MicrosoftDocs/win32/blob/docs/desktop-src/WmiSdk/wmi-error-constants.md))
-    #[error("HRESULT Call failed with: {hres:#X} {desc}", desc = hres::to_str(*hres))]
+    #[error(
+        "HRESULT Call failed with: {hres:#X} ({class})\n{message}{detail}", 
+        class = hres::to_class(*hres),
+        message = hres::to_message(*hres),
+        detail = hres::to_detail(*hres))
+    ]
     HResultError { hres: i32 },
     #[error(transparent)]
     ParseIntError(#[from] std::num::ParseIntError),
